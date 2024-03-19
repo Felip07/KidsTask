1454f552d5d65d7db1a840f9af36c9ec7abccc08
DSM4 <fatec-dsm4@academico.intra>
2024-03-18 22:53:32 -0300

    att2

--- exercicio-01/controllers/routes.py
+++ exercicio-01/controllers/routes.py
@@ -1,26 +1,37 @@
 from flask import render_template, request
 
-tarefa = []
+tarefas = []
 tarefalist = [{'Tarefa', 'Escovar os dentes'}]
+projeto = []
 
 def init_app(app):
     @app.route('/')
     def home():
         return render_template("index.html")
     
-    @app.route('/cadprojetos')
+    @app.route('/cadprojetos', methods=["GET","POST"])
     def cadproj():
-        return render_template("cadprojetos.html")
+        
+        if request.method == 'POST':
+            if request.form.get('projeto'):
+                projeto.append(request.form.get('projeto'))
+        return render_template("cadprojetos.html", projeto=projeto)
     
     @app.route('/cadtarefa', methods=['GET', 'POST'])
     def cadtarefa():
         
-        if request.method == 'POST':
-            if request.form.get('tarefa'):
-                tarefa.append(request.form.get('tarefa'))
         
-        return render_template("cadtarefa.html", tarefa=tarefa)
+        
+        if request.method == 'POST':
+                dado1 = request.form['Id']
+                dado2 = request.form['Tarefa']
+                dado3 = request.form['Descrição']
+                dado4 = request.form['Feita']
+                tarefas.append([dado1,dado2,dado3,dado4])
+                return render_template('cadtarefa.html', tarefas=tarefas)
+        
+        return render_template("cadtarefa.html", tarefas=tarefas)
     
-    @app.route('/config')
-    def config():
-        return render_template("config.html")
+    @app.route('/aboutus')
+    def aboutus():
+        return render_template("aboutus.html")
